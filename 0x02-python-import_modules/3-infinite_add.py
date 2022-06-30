9e298c59559f2327270a09253535c94bfcd44788
Sotonwa John <johnsotonwa@gmail.com>
2022-06-30 23:40:22 +0100

    Infinite add Fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    arg = sys.argv
-
-    sum = 0
-    if len(arg) > 1:
-        for i in arg:
-            if i != arg[0]:
-                sum += int(i)
-
-    print("{}".format(sum))
+    print(sum(int(i) for i in sys.argv[1:]))
